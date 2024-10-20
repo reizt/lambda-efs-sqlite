386f9dad17a73699023d6ca0d307768d26669eb7
Reiju Takahashi <reizt.dev@gmail.com>
2024-10-21 00:52:59 +0900

    🎉 initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.72.1"
+    }
+  }
+  required_version = ">= 1.9.8"
+
+  cloud {
+    organization = "crtm"
+    workspaces {
+      name = "lambda-efs-sqlite"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      app = "lambda-efs-sqlite"
+    }
+  }
+}
