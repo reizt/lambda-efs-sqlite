b7e0c7297365f630d5bcb9fed949085571a66043
Reiju Takahashi <reizt.dev@gmail.com>
2024-10-22 00:05:46 +0900

    ✨ gha upload-layer.yml

--- create-layer.sh
+++ create-layer.sh
@@ -6,7 +6,7 @@ cp .python-version requirements.lock requirements-dev.lock pyproject.toml README
 cd tmp
 
 python3.12 -m venv layer
-layer/bin/pip install -r requirements.lock --platform=manylinux2014_x86_64 --only-binary=:all: --target layer/lib/python3.12/site-packages
+layer/bin/pip install -r requirements.lock
 mv layer/lib/python3.12/site-packages python
 zip -r ../layer.zip python
 
